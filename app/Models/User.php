bb6be57fb61f8e26ee33ad598c6d525c8ab7e780
Alexey Kravchenko <alexey.kravchenko@spryker.com>
2026-01-30 21:31:24 +0200

    style: Remove blank line in User model.

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,7 +8,6 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-
 /**
  * @property UserRole $role
  */
